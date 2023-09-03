6dd982b2f73302320d50ffef1bdee23f61e21d88
Mriyam Tamuli <mbtamuli@gmail.com>
2023-09-03 19:51:22 +0530

    Remove unnecessary outputs
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- main.tf
+++ main.tf
@@ -185,14 +185,6 @@ resource "terraform_data" "tls_certificate" {
   }
 }
 
-output "ec2_public_ip" {
-  value = aws_instance.web.public_ip
-}
-
-output "ec2_public_dns" {
-  value = aws_instance.web.public_dns
-}
-
-output "rds_endpoint" {
-  value = aws_db_instance.wordpress.endpoint
+output "website_address" {
+  value = "https://${cloudflare_record.wordpress.hostname}"
 }
